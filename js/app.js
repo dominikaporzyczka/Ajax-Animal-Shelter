538ebd013b8b1e807dce3f8044444b8ba32e0325
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-04-18 22:50:27 +0200

    Add change of animal name in display function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let animalInfo;
+const animalsNav = document.querySelectorAll('.animals-list_animal');
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'data.json', true);
@@ -9,4 +10,14 @@ xhr.onload = function () {
     if (xhr.status === 200) {
         animalInfo = JSON.parse(xhr.responseText);
     }
+}
+
+for (let i = 0; i < animalsNav.length; i++) {
+    animalsNav[i].addEventListener('click', function () {
+        display(i);
+    });
+}
+
+function display(index) {
+    document.getElementById('animal_name').innerHTML = animalInfo[index].name;
 }
